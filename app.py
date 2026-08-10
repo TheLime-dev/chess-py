be44bf133decf8c586dd85fd09314c565c7acf60
emil <sunerya@hotmail.com>
2026-08-10 22:27:27 +0200

    added display of pieces

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template
+from flask import Flask, render_template, jsonify
 
 from main import Game
 
@@ -6,9 +6,16 @@ app = Flask(__name__)
 
 game = Game()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__=='__main__':
+
+@app.route('/board')
+def get_board():
+    return jsonify(game.board)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,7 @@
-const board = document.getElementById("chessboard");
+// Display the board
+
+
+const boardElement = document.getElementById("chessboard");
 const filelabels = document.getElementById("file-labels");
 const ranklabels = document.getElementById("rank-labels");
 
@@ -20,7 +23,7 @@ for (let row = 0; row < 8; row++) {
             square.classList.add("dark");
         }
 
-        board.appendChild(square);
+        boardElement.appendChild(square);
     }
 }
 
@@ -29,3 +32,42 @@ for (const file of files) {
     letter.textContent = file;
     filelabels.appendChild(letter);
 }
+
+// display pieces
+
+
+const pieces = {
+    "1": "p",
+    "2": "b",
+    "3": "n",
+    "4": "r",
+    "5": "q",
+    "6": "k"
+};
+
+async function showPieces() {
+    const response = await fetch('/board');
+    const board = await response.json();
+    const squares = boardElement.querySelectorAll(".square");
+
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const value = board[7 - row][col];
+            const square = squares[row * 8 + col];
+
+            if (value === 0) {
+                continue;
+            }
+            const color = value > 0 ? "w" : "b";
+            const pieceValue = color + pieces[Math.abs(value)];
+            const piece = document.createElement("img");
+            piece.src = "static/pieces/" + pieceValue + ".png";
+
+            square.appendChild(piece);
+
+        }
+    }
+
+}
+
+showPieces();
