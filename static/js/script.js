c62a9d40ca39f1a02475541f1d946a8f0a993771
emil <sunerya@hotmail.com>
2026-08-10 04:03:15 +0200

    added files and ranks to board

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,13 @@
 const board = document.getElementById("chessboard");
+const filelabels = document.getElementById("file-labels");
+const ranklabels = document.getElementById("rank-labels");
+
+const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 for (let row = 0; row < 8; row++) {
+    const number = document.createElement("div");
+    number.textContent = 8 - row;
+    ranklabels.appendChild(number);
     for (let col = 0; col < 8; col++) {
 
         const square = document.createElement("div");
@@ -15,4 +22,10 @@ for (let row = 0; row < 8; row++) {
 
         board.appendChild(square);
     }
+}
+
+for (const file of files) {
+    const letter = document.createElement("div");
+    letter.textContent = file;
+    filelabels.appendChild(letter);
 }
