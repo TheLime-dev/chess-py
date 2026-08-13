659c6ec092a17bd12a7a2471f3391773810f4922
emil <sunerya@hotmail.com>
2026-08-13 22:01:31 +0200

    tiny bug fix

--- static/js/script.js
+++ static/js/script.js
@@ -2,24 +2,10 @@ const boardElement = document.getElementById("chessboard");
 const filelabels = document.getElementById("file-labels");
 const ranklabels = document.getElementById("rank-labels");
 
-const BOARD = [
-    ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
-    ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
-    ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
-    ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
-    ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
-    ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
-    ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
-    ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
-];
+const BOARD = [["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"], ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"], ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"], ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"], ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"], ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"], ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"], ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],];
 
 const pieces = {
-    "1": "p",
-    "2": "b",
-    "3": "n",
-    "4": "r",
-    "5": "q",
-    "6": "k"
+    "1": "p", "2": "b", "3": "n", "4": "r", "5": "q", "6": "k"
 };
 
 const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
@@ -29,10 +15,9 @@ let selectedPosition = null;
 let board = [];
 let curTurn = 0;
 
+
+// squares
 for (let row = 0; row < 8; row++) {
-    const number = document.createElement("div");
-    number.textContent = 8 - row;
-    ranklabels.appendChild(number);
     for (let col = 0; col < 8; col++) {
 
         const square = document.createElement("div");
@@ -67,8 +52,7 @@ for (let row = 0; row < 8; row++) {
                 selectedSquare = square;
                 square.classList.add("selected");
                 selectedPosition = {
-                    row: 7 - row,
-                    col: col
+                    row: 7 - row, col: col
                 };
             } else {
                 if (pieceColor === curTurn) {
@@ -76,14 +60,12 @@ for (let row = 0; row < 8; row++) {
                     selectedSquare = square;
                     square.classList.add("selected");
                     selectedPosition = {
-                        row: 7 - row,
-                        col: col
+                        row: 7 - row, col: col
                     };
 
                 } else {
                     const target = {
-                        row: 7 - row,
-                        col: col
+                        row: 7 - row, col: col
                     };
                     makeMove(selectedPosition, target);
                     selectedSquare = null;
@@ -97,6 +79,17 @@ for (let row = 0; row < 8; row++) {
         boardElement.appendChild(square);
     }
 }
+
+
+//rank labels
+for (let row = 0; row < 8; row++) {
+    const number = document.createElement("div");
+    number.textContent = 8 - row;
+    ranklabels.appendChild(number);
+}
+
+
+// file labels
 
 for (const file of files) {
     const letter = document.createElement("div");
